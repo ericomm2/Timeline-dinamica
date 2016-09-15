5285b699d7be7e0b3268a7622cd68d2089ba708c
Érico Medeiros <erico.medeiros@gmail.com>
2016-09-15 20:57:27 -0300

    Adicionado sistema de acesso seguro, usando sessões de usuário

--- app/controllers/tuiter_controller.rb
+++ app/controllers/tuiter_controller.rb
@@ -6,23 +6,70 @@ class TuiterController < ApplicationController
     end
   end
 
-  def view_posts
+  def login
+    username=params[:user][:username]
+    password=Digest::MD5.hexdigest(params[:user][:password])
+    login = User.where("username = ? AND password = ?",username,password)
+    if login == nil
+      redirect_to("/")
+    else
+      session[:user_id] = login[0][:id]
+      redirect_to("/home")
+    end
   end
 
-  def create_user
-    if check_var(params[:user]) and check_var(params[:user][:email]) and check_var(params[:user][:password]) and check_var(params[:user][:username]) and check_var(params[:user][:bio]) and check_var(params[:user][:foto])
-      redirect_to "/" and return if check_user params[:user]
-      user = User.new
+  def logout
+    session[:user_id] = nil
+    redirect_to("/")
+  end
+
+  def edit_page
+    if ! check_var session[:user_id]
+      redirect_to("/")
+    else
+      @user = User.find(session[:user_id])
+    end
+  end
+
+  def edit_user
+    if ! check_var session[:user_id]
+      redirect_to("/")
+    else
+      user = User.find(session[:user_id])
       user.username = params[:user][:username]
       user.email = params[:user][:email]
       user.bio = params[:user][:bio]
       user.image = params[:user][:foto]
-      user.password = Digest::MD5.hexdigest(params[:user][:password])
       user.save
-      #session[:user_id] = user.id
-      redirect_to("/#{params[:user][:username]}")
+      redirect_to("/home")
+    end
+  end
+
+  def view_posts
+    if ! check_var session[:user_id]
+      redirect_to("/")
     else
-      redirect_to(home)
+      @user = User.find(session[:user_id])
+    end
+  end
+
+  def create_user
+    if check_var(params[:user]) and check_var(params[:user][:email]) and check_var(params[:user][:password]) and check_var(params[:user][:username]) and check_var(params[:user][:bio]) and check_var(params[:user][:foto])
+      if check_user params[:user]
+        redirect_to "/"
+      else
+        user = User.new
+        user.username = params[:user][:username]
+        user.email = params[:user][:email]
+        user.bio = params[:user][:bio]
+        user.image = params[:user][:foto]
+        user.password = Digest::MD5.hexdigest(params[:user][:password])
+        user.save
+        #session[:user_id] = user.id
+        redirect_to("/#{params[:user][:username]}")
+      end
+    else
+      redirect_to("/")
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   get '/', to: 'tuiter#home'
   post '/', to: 'tuiter#create_user'
   get '/home', to: 'tuiter#view_posts'
+  post '/login', to: 'tuiter#login'
+  get '/logout', to: 'tuiter#logout'
+  get '/edit', to: 'tuiter#edit_page'
+  post '/edit', to: 'tuiter#edit_user'
+  post '/password', to: 'tuiter#edit_password'
   get '/:username', to: 'tuiter#view_user'
 end
